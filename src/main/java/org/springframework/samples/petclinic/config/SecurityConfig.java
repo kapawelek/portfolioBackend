2c7ffaaab2b7e820bf68cabd68c0254d396974bc
karol <karol.pawelek2@gmail.com>
2025-11-09 22:53:34 +0100

    frontend visibility config

--- src/main/java/org/springframework/samples/petclinic/config/SecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/config/SecurityConfig.java
@@ -1,28 +1,36 @@
 package org.springframework.samples.petclinic.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
+@ConditionalOnProperty(
+    name = "app.security.enabled",
+    havingValue = "true",
+    matchIfMissing = true // jeśli nie ustawisz property, security będzie WŁĄCZONE
+)
 public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean(name = "securityFilterChain")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(Customizer.withDefaults())
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(reg -> reg
                 .requestMatchers(
-                    "/v3/api-docs/**", "/swagger-ui/**",
-                    "/actuator/**",
-                    "/petclinic/api/**"
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/actuator/**"
                 ).permitAll()
-                .anyRequest().permitAll()
-            );
-
+                .anyRequest().authenticated()
+            )
+            .httpBasic(basic -> {
+            });
         return http.build();
     }
 }
